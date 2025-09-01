25f5c3be8def17784bfaff1d7999a84127c8f32c
Ivan Lakoza <ivan.lakoza@gmail.com>
2025-09-01 21:38:02 +0300

    Auto-deploy: vitrina-sync-ym-on-all-pages-2025-09-01.zip @ 2025-09-01 21:38:02

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,34 @@
-import './globals.css';
-import React from 'react';
+import "./globals.css";
+import React from "react";
+import Script from "next/script";
 
 export const metadata = {
-  title: 'Витрина',
+  title: "Витрина",
+  icons: { icon: "/icon.svg" }, // favicon для всех страниц
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
       <body className="bg-gray-50 text-gray-900">
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103973054', 'ym');
+            ym(103973054, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<div><img src="https://mc.yandex.ru/watch/103973054" style="position:absolute; left:-9999px;" alt="" /></div>',
+          }}
+        />
+        {/* /Yandex.Metrika counter */}
+
         {children}
       </body>
     </html>
